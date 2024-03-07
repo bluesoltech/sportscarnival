209f96f8cb5c694c7e127cddfa11cd6b230eadf7
bluesoltech-kunal <dev.kunal.bluesoltech@gmail.com>
2024-03-07 14:26:44 +0530

    added routes

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -35,43 +35,31 @@ function Nav({ className }) {
       <Menu setActive={setActive}>
         <MenuItem setActive={setActive} active={active} item="Home">
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/hobby">Upcoming Carnival</HoveredLink>
-            <HoveredLink href="/individual">Team</HoveredLink>
+            <HoveredLink to="/">Upcoming Carnival</HoveredLink>
+            <HoveredLink to="/team">Team</HoveredLink>
           </div>
         </MenuItem>
         <MenuItem setActive={setActive} active={active} item="Activities">
           <div className="  text-sm grid grid-cols-2 gap-10 p-4">
             <ProductItem
-              title="Day 1"
-              href="https://algochurn.com"
+              title="Art & Crafts"
+              href="/activity/art"
               src="https://assets.aceternity.com/demos/algochurn.webp"
               description="Prepare for tech interviews like never before."
             />
             <ProductItem
-              title="Day 2"
-              href="https://tailwindmasterkit.com"
+              title="Adventure"
+              href="/activity/adventure"
               src="https://assets.aceternity.com/demos/tailwindmasterkit.webp"
               description="Production ready Tailwind css components for your next project"
-            />
-            <ProductItem
-              title="Day 3"
-              href="https://gomoonbeam.com"
-              src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.51.31%E2%80%AFPM.png"
-              description="Never write from scratch again. Go from idea to blog in minutes."
-            />
-            <ProductItem
-              title="Day 4"
-              href="https://userogue.com"
-              src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.47.07%E2%80%AFPM.png"
-              description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
             />
           </div>
         </MenuItem>
         <MenuItem setActive={setActive} active={active} item="Contact Us">
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/hobby">Exhibitor</HoveredLink>
-            <HoveredLink href="/individual">Volunteer</HoveredLink>
-            <HoveredLink href="/team">Contact</HoveredLink>
+            <HoveredLink to="/exhibitor">Exhibitor</HoveredLink>
+            <HoveredLink to="/volunteer">Volunteer</HoveredLink>
+            <HoveredLink to="/contact">Contact</HoveredLink>
           </div>
         </MenuItem>
       </Menu>
