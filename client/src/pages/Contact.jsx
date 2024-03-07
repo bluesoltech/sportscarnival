dd9d09333074f5e5f365097fa8133fe0609b1c18
ghetiyabansi <bansip464@gmail.com>
2024-03-07 16:49:25 +0530

    adventure

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect } from 'react';
 
 function Contact() {
-  return <div>Contact</div>;
-}
+  useEffect(() => {
+    // Create a script element
+    const script = document.createElement('script');
+    script.src = '//embed.typeform.com/next/embed.js';
+    script.async = true;
+
+    // Append the script to the body
+    document.body.appendChild(script);
+
+    // Cleanup the script when the component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []); // Empty dependency array means this effect will only run once
+
+  return (
+    <div data-tf-live="01HRC55MXD968DEZNX8EX0G6RB"></div>
+  )
+};
+
 
 export default Contact;
--- client/src/pages/Volunteer.jsx
+++ client/src/pages/Volunteer.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect } from 'react';
 
 function Volunteer() {
-  return <div>Volunteer</div>;
+  useEffect(() => {
+    // Create a script element
+    const script = document.createElement('script');
+    script.src = '//embed.typeform.com/next/embed.js';
+    script.async = true;
+
+    // Append the script to the body
+    document.body.appendChild(script);
+
+    // Cleanup the script when the component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+  return (
+    <div data-tf-live="01HRC440NABCS6ASX8FT7RA9V7"></div>
+  )
 }
 
 export default Volunteer;
